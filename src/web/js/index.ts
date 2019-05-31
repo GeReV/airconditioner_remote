1c05b14f303e86ffe482492266b9341749764eba
Amir Grozki <amirgrozki@gmail.com>
2019-06-01 01:21:07 +0300

    Removed all frontend files.

--- src/web/js/index.ts
+++
@@ -1,8 +0,0 @@
-import { on, $ } from "./utils";
-import Toggle from "./toggle";
-
-import '../scss/index.scss';
-
-on(document, 'DOMContentLoaded', () => {
-    new Toggle($('#power'), 'active');
-});
